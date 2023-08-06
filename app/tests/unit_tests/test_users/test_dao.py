1b264d2b44ce8e01e0237875240c873996ddfc33
Kostachy <kostachy@gmail.com>
2023-08-06 23:34:19 +0300

    new tests

---
+++ app/tests/unit_tests/test_users/test_dao.py
@@ -0,0 +1,18 @@
+from app.user.dao import UsersDAO
+import pytest
+
+
+@pytest.mark.parametrize('user_id,email,is_exist', [
+    (1, 'test@test.com', True),
+    (2, 'krut@example.com', True),
+    (3, 'fdfdfdf', False),
+])
+async def test_find_user_by_id(user_id, email, is_exist):
+    user = await UsersDAO.get_by_id(user_id)
+
+    if is_exist:
+        assert user
+        assert user.id == user_id
+        assert user.email == email
+    else:
+        assert not user
